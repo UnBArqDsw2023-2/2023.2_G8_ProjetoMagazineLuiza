2d6d59d20d07ebe7e4f57e8bf8d572f5745d08f1
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2023-12-01 18:53:27 -0300

    implementacao do facade nas rotas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,63 @@
 import { ProductReviewRestController } from "./adapters/productReviewRestController";
 import { ProductReviewSoapController } from "./adapters/productReviewSoapController";
 import { ProductReviewService } from "./application/services/productReviewService";
-import { ProductReviewRepositoryImp } from "./infrastructure/repositories/productReviewRepository"; 
+import { ProductReviewRepositoryImp } from "./infrastructure/repositories/productReviewRepository";
 
-const productReviewRepositoryImp = new ProductReviewRepositoryImp()
+class ProductReviewRouteFacade {
+  private productReviewRestController: ProductReviewRestController;
+  private productReviewSoapController: ProductReviewSoapController;
 
-const productReviewService = new ProductReviewService(productReviewRepositoryImp)
+  constructor() {
+    const productReviewRepositoryImp = new ProductReviewRepositoryImp();
+    const productReviewService = new ProductReviewService(productReviewRepositoryImp);
 
-const productReviewRestController = new ProductReviewRestController(productReviewService)
+    this.productReviewRestController = new ProductReviewRestController(productReviewService);
+    this.productReviewSoapController = new ProductReviewSoapController(productReviewService);
+  }
 
-const productReviewSoapController = new ProductReviewSoapController(productReviewService)
+  async handleProductReviewRequest(req: Request): Promise<Response> {
+    const response = await this.productReviewRestController.getProductReviews(req);
+    return new Response(`${JSON.stringify(response)}`);
+  }
+
+  async handleCreateProductReviewRequest(req: Request): Promise<Response> {
+    if (req.body) {
+      const response = await this.productReviewRestController.rateProductById(req);
+      return new Response(`${response}`);
+    } else {
+      return new Response("Missing request body");
+    }
+  }
+
+  async handleCreateProductSoapRequest(req: Request): Promise<Response> {
+    const response = await this.productReviewSoapController.rateProductById(req);
+    return new Response(`${response}`);
+  }
+
+  async handleProductReviewSoapRequest(req: Request): Promise<Response> {
+    const response = await this.productReviewSoapController.getProductReviews(req);
+    return new Response(JSON.stringify(response));
+  }
+}
 
 Bun.serve({
-  async fetch(req: Request){
+  async fetch(req: Request) {
+    const routeFacade = new ProductReviewRouteFacade();
+
     const url = new URL(req.url);
     const pathname = url.pathname;
 
-    if (pathname === "/productReview") {
-      const response = productReviewRestController.getProductReviews(req)
-      return new Response(`${JSON.stringify(response)}`);
-    } else if (pathname === "/createProductReview" && req.body){
-      const response = await productReviewRestController.rateProductById(req);
-      return new Response(`${response}`);
-    } else if (pathname === "/createProductSoap") {
-      const response = await productReviewSoapController.rateProductById(req);
-      return new Response(`${response}`);
-    } else if (pathname === "/productReviewSoap"){
-      const response = await productReviewSoapController.getProductReviews(req);
-      return new Response(JSON.stringify(response));
-    } else {
-      return new Response("");
+    switch (pathname) {
+      case "/productReview":
+        return routeFacade.handleProductReviewRequest(req);
+      case "/createProductReview":
+        return routeFacade.handleCreateProductReviewRequest(req);
+      case "/createProductSoap":
+        return routeFacade.handleCreateProductSoapRequest(req);
+      case "/productReviewSoap":
+        return routeFacade.handleProductReviewSoapRequest(req);
+      default:
+        return new Response("Invalid route");
     }
   }
-})
+});
